7d5338d8aea1e90c85a157df5eadc1b978fe50d5
ndragun92 <nemanjadragun92@gmail.com>
2022-11-24 12:24:24 +0100

    feat: add PWA support

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,4 +14,25 @@ export default defineNuxtConfig({
             autoprefixer: {},
         },
     },
+    modules: [
+        '@kevinmarrec/nuxt-pwa'
+    ],
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    pwa: {
+        meta: {
+            name: 'Learn Letters',
+            author: 'Nemanja Dragun',
+            lang: 'en',
+            theme_color: 'white'
+        },
+        manifest: {
+            name: 'Learn Letters',
+            short_name: 'learn_letters',
+            theme_color: 'white'
+        },
+        icon: {
+            fileName: 'logo.svg'
+        },
+    }
 })
